710abe1af6d7d8fa0dbdb20676f721599d7fcfd7
arsheelsheikh <sheikharsheel@gmail.com>
2023-08-12 22:17:00 +0530

    implemented animated profile sections

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Container, Box, Heading, Image } from '@chakra-ui/react';
+import { 
+  Container, 
+  Box,
+  Heading,
+  Image, 
+  useColorModeValue 
+} from '@chakra-ui/react';
+import Section from '../components/section'
+import Head from 'next/head';
 
 const Page = () => {
     return(
       <Container>
-        <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
             Hello 👋, I&apos;m Arsheel Sheikh full stack developer and designer!
         </Box>
 
@@ -32,6 +40,13 @@ const Page = () => {
           />
         </Box>
         </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-tile">
+            Work
+          </Heading>
+          <p>Paragraph</p>
+        </Section>
       </Container>  
     ) 
 };
